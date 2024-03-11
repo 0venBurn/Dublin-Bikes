ce99c70b5c1397cbd901b591bf94db276f337cfb
Evan Byrne <evanbyrnecode@gmail.com>
2024-03-11 00:34:11 +0000

    type: feat Added API endpoints for the flask app and tested fetch api on front end

--- app/main.py
+++ app/main.py
@@ -10,26 +10,36 @@ page with current weather data.
 import os
 
 from flask import Blueprint, render_template
+from flask_cors import CORS
 
 from .routes.data_fetcher import get_latest_weather_data, get_stations_data
 
 # Create a Blueprint instance for the main module.
 main = Blueprint("main", __name__)
+CORS(main)
 
 
 @main.route("/")
 def index():
     """Renders the main page."""
-    # Fetch the latest weather data from the database.
-    weather_data = get_latest_weather_data()
-    # Fetch stations and their latest availability using the relationship
-    stations_data = get_stations_data()
     # Fetch the Google Maps API key from environment variables
     google_maps_api_key = os.getenv("GOOGLE_MAPS_API", "")
 
     return render_template(
         "index.html",
-        weather=weather_data,
-        stations=stations_data,
         google_maps_api_key=google_maps_api_key,
     )
+
+
+@main.route("/api/weather")  # type: ignore
+def weather_data():
+    """Api Endpoint for fetching weather data from database."""
+    data = get_latest_weather_data()
+    return data
+
+
+@main.route("/api/stations")
+def stations_data():
+    """API endpoint for fetching recent station data from database."""
+    data = get_stations_data()
+    return data
