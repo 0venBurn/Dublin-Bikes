2b69ecfd89e0c0825db986a19046ef49535e8ba8
Evan Byrne <evanbyrnecode@gmail.com>
2024-03-11 00:35:42 +0000

    type: feat Created endpoints

--- app/configs/base.py
+++ app/configs/base.py
@@ -15,9 +15,12 @@ class Config:
     PASSWORD = os.getenv("DB_PASSWORD")
     DATABASE = os.getenv("DB")
     HOST = os.getenv("HOST")
+    LOCAL_PORT = os.getenv("LOCAL_PORT")
 
     # This URI contains the database connection information required by SQLAlchemy.
-    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{USERNAME}:{PASSWORD}@{HOST}/{DATABASE}"
+    SQLALCHEMY_DATABASE_URI = (
+        f"mysql+pymysql://{USERNAME}:{PASSWORD}@localhost:{LOCAL_PORT}/{DATABASE}"
+    )
 
     # Disable the SQLAlchemy event system, which can lead to significant overhead if not needed.
     SQLALCHEMY_TRACK_MODIFICATIONS = False
